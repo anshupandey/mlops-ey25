a2384601ece7162a5daa10c6d2c07394374ec2b4
Anshu Pandey <anshuspandey@gmail.com>
2025-03-06 05:28:41 +0000

    modified workflow

--- src/model/train.py
+++ src/model/train.py
@@ -21,7 +21,7 @@ def main(args):
     X_train, X_test, y_train, y_test = split_data(df)
 
     # train model
-    train_model(args.reg_rate, X_train, X_test, y_train, y_test)
+    model = train_model(args.reg_rate, X_trai, y_train)
 
 
 def get_csvs_df(path):
@@ -40,9 +40,10 @@ def split_data(df):
     return X_train,X_test,y_train,y_test
 
 
-def train_model(reg_rate, X_train, X_test, y_train, y_test):
+def train_model(reg_rate, X_train, y_train):
     # train model
-    LogisticRegression(C=1/reg_rate, solver="liblinear").fit(X_train, y_train)
+    model = LogisticRegression(C=1/reg_rate, solver="liblinear").fit(X_train, y_train)
+    return model
 
 
 
