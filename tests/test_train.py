f2470cec12ff47f4994caa2ffe5ba67c405e538b
Anshu Pandey <anshuspandey@gmail.com>
2025-03-06 05:14:17 +0000

    modified workflow

--- tests/test_train.py
+++ tests/test_train.py
@@ -1,6 +1,7 @@
-from model.train import get_csvs_df
+from model.train import get_csvs_df, train_model
 import os
 import pytest
+import numpy as np
 
 
 def test_csvs_no_files():
@@ -20,3 +21,11 @@ def test_csvs_creates_dataframe():
     datasets_directory = os.path.join(current_directory, 'datasets')
     result = get_csvs_df(datasets_directory)
     assert len(result) == 20
+
+
+def test_train_model():
+    xtrain = np.array([1,2,3,4,5,6]).reshape(-1,1)
+    ytrain = np.array([0,0,0,1,1,1])
+    reg_model = train_model(0.02,xtrain,ytrain)
+    preds = reg_model.predict([[8],[2]])
+    np.testing.assert_almost_equal(preds,[1,0])
